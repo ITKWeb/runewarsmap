fd6da0a57f30df4873a6111f16529207c9b2b646
bastien <bastien.amar@itkweb.com>
2013-05-28 13:49:16 +0200

    Quand on clique sur OK, ca zappe de vue !

--- RunewarsMap/src/com/itk/hday/android/WelcomeActivity.java
+++ RunewarsMap/src/com/itk/hday/android/WelcomeActivity.java
@@ -3,6 +3,11 @@ package com.itk.hday.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.itk.hday.R;
 import com.itk.hday.util.SystemUiHider;
@@ -18,7 +23,24 @@ public class WelcomeActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+//		Spinner spinner = (Spinner) findViewById(R.id.nbPlayerSpinner);
+//		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, 
+//				R.array.nbPlayerStringArray, 
+//				android.R.layout.simple_spinner_item);
+//		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//		spinner.setAdapter(adapter);
 		setContentView(R.layout.activity_welcome);
+		
+		final Button button = (Button) findViewById(R.id.validateConfigButton);
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				setContentView(R.layout.runwars_gameboard);
+			}
+		});
 	}
 
 //	@Override
